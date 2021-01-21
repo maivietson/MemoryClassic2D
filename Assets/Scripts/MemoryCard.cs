10108badf356f29cae8d17fb00af2d41fd6565fa
Sonmv <maivietson.nd@gmail.com>
2021-01-21 08:25:46 +0700

    +[son.mv][Unity2D]: Implement manager card and flipper card

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private Sprite image;
+    [SerializeField] private SceneController sceneController;
+
+    private int _id;
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    private void Start()
+    {
+        //GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    private void OnMouseDown()
+    {
+        if(cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
+    }
+}
